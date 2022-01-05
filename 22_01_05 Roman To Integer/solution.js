cbcae5283f6b78c9d01681c3e33117c68d2dce4c
Ian <ianswensson@gmail.com>
2022-01-05 10:49:14 -0700

    fixed solution

--- 22_01_05 Roman To Integer/solution.js
+++ 22_01_05 Roman To Integer/solution.js
@@ -15,7 +15,11 @@ var romanToInt = function(s) {
   for (var i = 0; i < s.length; i ++) {
     // if current character is I, X, or C
       // check the following character for V, L, or D respectively
-    if ((s[i] === 'I' && s[i + 1] === 'V') || (s[i] === 'X' && s[i + 1] === 'L') || (s[i] === 'C' && s[i + 1] === 'D') ) {
+    if (
+      (s[i] === 'I' && (s[i + 1] === 'V' || s[i + 1] === 'X')) || 
+      (s[i] === 'X' && (s[i + 1] === 'L' || s[i + 1] === 'C')) || 
+      (s[i] === 'C' && (s[i + 1] === 'D' || s[i + 1] === 'M'))
+      ) {
       result -= romanValues[s[i]]
     } else {
       // get value of current character and add to result
